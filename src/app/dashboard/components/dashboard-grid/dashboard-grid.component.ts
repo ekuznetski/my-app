3846f15bffea652a88cd56036c9dffd459743389
ekuznetski <e.kuznetski@gmail.com>
2023-02-19 01:39:52 +0200

    remove trackby and ChangeDetectionStrategy

--- src/app/dashboard/components/dashboard-grid/dashboard-grid.component.ts
+++ src/app/dashboard/components/dashboard-grid/dashboard-grid.component.ts
@@ -1,15 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { DashboardStateModel, DashboardWidget } from "@app/dashboard/+state/dashboard.state";
 import { Select } from "@ngxs/store";
 import { GridsterConfig } from "angular-gridster2";
-import * as _ from "lodash";
 import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-dashboard-grid',
   templateUrl: './dashboard-grid.component.html',
   styleUrls: ['./dashboard-grid.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardGridComponent {
   @Select() dashboard$!: Observable<DashboardStateModel>;
@@ -17,19 +15,10 @@ export class DashboardGridComponent {
   options!: GridsterConfig;
   dashboard!: Array<DashboardWidget>;
 
-  trackByFn(index: number, item: DashboardWidget) {
-    return item.cols + item.rows + item.x + item.y;
-  }
 
   constructor() {
     this.dashboard$.subscribe((value) => {
-      console.log(value?.currentLayout?.widgets?.length, this.dashboard?.length);
-      if (
-        value &&
-        value?.currentLayout?.widgets?.length !== this.dashboard?.length
-      ) {
-        this.dashboard = [..._.cloneDeep(value.currentLayout.widgets)];
-      }
+      this.dashboard = value.currentLayout.widgets;
     });
   }
 }
